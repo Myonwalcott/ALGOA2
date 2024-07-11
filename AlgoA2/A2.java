d0f4f4fa37effcc2c201db25762f49a454b088b9
Myon Section D <N01660463@humber.ca>
2024-07-11 15:14:38 -0400

    Update A2.java

---
+++ AlgoA2/A2.java
@@ -0,0 +1,9 @@
+abstract class PCComponent {
+    protected String name;
+    protected double cost;
+
+    public PCComponent(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+}
